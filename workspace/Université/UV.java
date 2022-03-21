a3e5ecc77cb04c172f9239e48c8854776d37a612
Maxime <91959894+MaximeMonterin@users.noreply.github.com>
2022-03-21 23:15:41 +0100

    Delete workspace/Université directory

--- workspace/Université/UV.java
+++
@@ -1,7 +0,0 @@
-public enum UV {
-	MATHS,
-	JAVA,
-	SQL,
-	ANGLAIS,
-	HTML;
-}
--- workspace/Université/main.java
+++
@@ -1,9 +0,0 @@
-
-public class main {
-
-	public static void main(String[] args) {
-		Personnel Hugo = new Personnel(12, "Hugo", "Domage", "13 avenue de la fleur", "hugo.domage@gmail.com", "IUT AIX");
-		System.out.println(Hugo.toString());
-	}
-
-}
